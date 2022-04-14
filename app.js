e7b4666ba4e0b4f5586e267d1225279ffdec3faf
Gustavo Henrique Bernardo <gustavobernardo094@gmail.com>
2022-04-14 14:24:36 -0300

    Finished the challenge
    added the JavaScript portion of the project
    
    bring the buttons and all elements from the UI to the JavaScript
    
    created an event listener to the submit button, to change the page
    to a “Thank You” screen with the selected rating

--- app.js
+++ app.js
@@ -1,2 +1,35 @@
 const cardUI = document.querySelector('.card')
-console.log(cardUI, 'test')
+const rating = document.querySelectorAll('.rating button')
+const submitBtn = document.querySelector('.btn')
+
+let actualRate = 0
+
+rating.forEach(rate => {
+  rate.addEventListener('click', (e) => {
+    if (!e.target.classList.contains('active')) {
+      removeActive()
+      e.target.classList.add('active')
+
+      actualRate = e.target.innerText
+    }
+  })
+})
+
+submitBtn.addEventListener('click', () => {
+  cardUI.innerHTML = `
+  <div class="ty-card">
+    <img class="ty-image" src="./images/illustration-thank-you.svg" alt="" />
+    <p class="rating">You selected out ${actualRate} of 5 </p>
+    <h2 class="title">Thank You!</h2>
+    <p class="text">We appreciate you taking the time to give a rating. If
+    you ever need more support, don’t hesitate to get in touch!</p>
+   </div> 
+  `
+
+})
+
+function removeActive() {
+  for (let i = 0; i < rating.length; i++) {
+    rating[i].classList.remove('active')
+  }
+}
